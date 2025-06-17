a2909f0736472c21750b753af7fa138fa1527ac5
lucax13 <lucas13lima2006@gmail.com>
2025-06-17 10:31:03 -0300

    funcao

---
+++ dist/funcoes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function saudacao(nome) {
+    return `Olá ${nome}!`;
+}
+console.log(saudacao("lucas"));
---
+++ funcoes.ts
@@ -0,0 +1,8 @@
+//03-funcoes.ts
+export {};
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}!`;
+}
+
+console.log(saudacao("lucas"));
