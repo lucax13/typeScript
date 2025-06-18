17c8f913b8818eef08269f4dddda67f3fb744ab5
lucax13 <lucas13lima2006@gmail.com>
2025-06-18 09:44:13 -0300

    tuplas

--- 06-tuplas.ts
+++ 06-tuplas.ts
@@ -1,3 +1,5 @@
+import { separador } from "./modulos";
+
 export {};
 
 /*tuplas
@@ -12,3 +14,26 @@ console.log(usuario[2] ? "ativo" : "inativo");
 
 usuario[1] = 30;
 console.log(usuario[1] + "anos.");
+
+separador();
+
+type Produto = [string, number];
+
+const produtoA: Produto = ["Bermuda", 23.89];
+//const produtoB:  = [89, "camiseta"]
+const produtoC: Produto = ["iphone", 1000];
+
+console.log(produtoA);
+console.log(produtoC);
+
+separador();
+
+function verificarOperacao(sucesso: boolean) {
+  if (sucesso) {
+    return [200, "Operação realizada com sucesso!"];
+  }
+  return [500, "Erro inesperao no servidor"];
+}
+
+let resultadoDaOperacao = verificarOperacao(false);
+console.log(resultadoDaOperacao);
--- dist/06-tuplas.js
+++ dist/06-tuplas.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const modulos_1 = require("./modulos");
 /*tuplas
 É um tipo de array especial usada quando precisamos armazenar um con junto fixo
 de elementos de tipos diferentes*/
@@ -9,3 +10,18 @@ console.log(usuario[1] + " anos");
 console.log(usuario[2] ? "ativo" : "inativo");
 usuario[1] = 30;
 console.log(usuario[1] + "anos.");
+(0, modulos_1.separador)();
+const produtoA = ["Bermuda", 23.89];
+//const produtoB:  = [89, "camiseta"]
+const produtoC = ["iphone", 1000];
+console.log(produtoA);
+console.log(produtoC);
+(0, modulos_1.separador)();
+function verificarOperacao(sucesso) {
+    if (sucesso) {
+        return [200, "Operação realizada com sucesso!"];
+    }
+    return [500, "Erro inesperao no servidor"];
+}
+let resultadoDaOperacao = verificarOperacao(false);
+console.log(resultadoDaOperacao);
